7b0dce13bcc17fbe76bc6d292bf9c0afb0524476
elec-droid <elec-droid@example.com>
2020-11-24 03:47:19 -0800

    Скорректировано по замечениям + варианты решения преподователя

--- topic-5/5-HW.sql
+++ topic-5/5-HW.sql
@@ -1,21 +1,25 @@
 -- 1.1 Пусть в таблице users поля created_at и updated_at оказались незаполненными. Заполните их текущими датой и временем.
 UPDATE users SET created_at = NOW(); -- updated_at обновится автоматически
 
+
 -- 1.2 
 /* Таблица users была неудачно спроектирована. Записи created_at и updated_at были 
  * заданы типом VARCHAR и в них долгое время помещались значения в формате 20.10.2017 8:10. 
  * Необходимо преобразовать поля к типу DATETIME, сохранив введённые ранее значения.
  */
 DROP TABLE IF EXISTS time_created;
-CREATE TEMPORARY TABLE time_created (created_at DATETIME, updated_at DATETIME);
-INSERT INTO time_created SELECT STR_TO_DATE(created_at, '%d.%m.%Y %h:%i'), STR_TO_DATE(updated_at, '%d.%m.%Y %h:%i') FROM users;
+CREATE TEMPORARY TABLE time_created (id INT UNSIGNED, created_at DATETIME, updated_at DATETIME);
+INSERT INTO time_created SELECT id, STR_TO_DATE(created_at, '%d.%m.%Y %h:%i'), STR_TO_DATE(updated_at, '%d.%m.%Y %h:%i') FROM users;
 UPDATE users SET created_at = NULL, updated_at = NULL;
 ALTER TABLE users 
 	MODIFY COLUMN created_at DATETIME,
 	MODIFY COLUMN updated_at DATETIME;
-UPDATE users SET created_at = (SELECT created_at FROM time_created);
-UPDATE users SET updated_at = (SELECT updated_at FROM time_created);
+UPDATE users SET created_at = (SELECT created_at FROM time_created WHERE time_created.id = users.id);
+UPDATE users SET updated_at = (SELECT updated_at FROM time_created WHERE time_created.id = users.id);
 DROP TABLE time_created;
+
+SELECT * FROM time_created LIMIT 10;
+
 
 -- 1.3 
 /* В таблице складских запасов storehouses_products в поле value могут встречаться самые 
@@ -24,6 +28,10 @@ DROP TABLE time_created;
  * увеличения значения value. Однако нулевые запасы должны выводиться в конце, после всех
  */
 SELECT * FROM storehouses_products ORDER BY CASE WHEN value = 0 THEN 4294967295 ELSE value END
+
+-- GB
+-- SELECT * FROM storehouses_products ORDER BY value = 0, value;
+
 
 -- 1.4
 /* (по желанию) Из таблицы users необходимо извлечь пользователей, родившихся в августе и
@@ -42,8 +50,13 @@ CASE
     WHEN media_type_id = 2 THEN 2
 END;
 
+-- Вариант GB
+-- SELECT * FROM media WHERE id IN(5, 1, 2) ORDER BY FIELD(id, 5, 1, 2);
+
+
 -- 2.1 Подсчитайте средний возраст пользователей в таблице users.
 SELECT FLOOR(AVG(TIMESTAMPDIFF(YEAR, birthday, NOW()))) FROM profiles;
+
 
 -- 2.2 
 /* Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели.
@@ -56,6 +69,7 @@ GROUP BY
     day_of_year
 ORDER BY
 	count_birthday DESC;
+
 
 -- 2.3 (по желанию) Подсчитайте произведение чисел в столбце таблицы.
 SELECT FLOOR(EXP(SUM(LOG(id)))) FROM media_types;
